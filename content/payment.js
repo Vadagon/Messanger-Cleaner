b9a04fe79cc8faefd47c521741adca1096a7bbf2
Vlad <verbalike@gmail.com>
2023-11-28 22:25:25 +0100

    price updated + fixed some shit

--- content/payment.js
+++ content/payment.js
@@ -1,4 +1,4 @@
-function insertPayment(){
+function insertPayment() {
     $(`<div id="payRequestUnfriender">
                 <div>
                     <div>
@@ -17,19 +17,29 @@ function insertPayment(){
                             </h2>
                             <div class="MassUnfrienderPlans">
                                 <label>Plans available</label>
-                                <p>Monthly subscription <a href="https://node.verblike.com/messengercleaner/oneTime/month">$4</a></p>
-                                <p class="specialOffer">Special 24h access <span>limited time offer</span> <a href="https://node.verblike.com/messengercleaner/oneTime/oneday" class="specialYellowButton">$1.49</a></p>
+                                <p>Monthly subscription <a href="https://node.verblike.com/messengercleaner/oneTime/month">$3.99</a></p>
+                                <p class="specialOffer">Unlimited access <span>limited time offer</span> <a href="https://node.verblike.com/messengercleaner/oneTime/oneday" class="specialYellowButton">$5.99</a></p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>`).appendTo('body').on('click', 'button', function(event){
-                event.preventDefault()
-                chrome.extension.sendMessage({email: $(this).parent().find('input').val()}, (e)=>{
-                    if(e==true){
-                        $('#payRequestUnfriender').remove()
-                    }
-                })
-            })
+            </div>`).appendTo('body').on('click', 'button', function (event) {
+        event.preventDefault()
+        checkPayment($(this).parent().find('input').val(), (e) => {
+            if (e == true) {
+                $('#payRequestUnfriender').remove()
+                startScript();
+            }
+        })
+    })
 }
 insertPayment()
+
+
+function checkPayment(email, cb) {
+    $.get('https://us-central1-massunfriender.cloudfunctions.net/DBinsert/isMember?email=' + email).done((e) => {
+        cb(e && e.result)
+    }).fail(() => {
+        cb(false)
+    })
+}
--- popup.js
+++ popup.js
@@ -1,16 +1,14 @@
 var a = {
     purchased: undefined
 }
-chrome.extension.sendMessage({type: 'data'}, (e)=>{
-    a = e;
-})
+
 class PopupActions {
     constructor() {
         this.pageFacebook = "https://www.facebook.com/messages/?locale=en_US";
     }
 
     isMessagePage(url) {
-        if(url.indexOf("https://www.facebook.com/messages/") != -1){
+        if (url.indexOf("https://www.facebook.com/messages/") != -1) {
             return true;
         } else {
             return false;
@@ -19,14 +17,27 @@ class PopupActions {
 
     removeMessages() {
         chrome.windows.getCurrent(function (currentWindow) {
-            chrome.tabs.query({active: true, windowId: currentWindow.id}, function (activeTabs) {
+            chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
                 activeTabs.map(function (tab) {
-                    chrome.tabs.insertCSS(tab.id, {file: 'assets/css/confirm.css', allFrames: false})
-                    chrome.tabs.insertCSS(tab.id, {file: 'assets/css/content.css', allFrames: false})
-                    chrome.tabs.executeScript(tab.id, {file: 'libs/jquery.js', allFrames: false});
-                    chrome.tabs.executeScript(tab.id, {file: 'libs/jquery-confirm.min.js', allFrames: false});
-                    if(a.purchased) chrome.tabs.executeScript(tab.id, {file: 'content/script.js', allFrames: false});
-                    else chrome.tabs.executeScript(tab.id, {file: 'content/payment.js', allFrames: false});
+                    chrome.scripting.insertCSS({
+                        target: { tabId: tab.id },
+                        files: ["assets/css/confirm.css", "assets/css/content.css"]
+                    });
+                    chrome.scripting.executeScript({
+                        target: { tabId: tab.id },
+                        files: [
+                            "libs/jquery.js",
+                            "libs/jquery-confirm.min.js",
+                            "content/script.js",
+                            "content/payment.js"
+                        ]
+                    })
+                    // chrome.tabs.insertCSS(tab.id, { file: 'assets/css/confirm.css', allFrames: false })
+                    // chrome.tabs.insertCSS(tab.id, { file: 'assets/css/content.css', allFrames: false })
+                    // chrome.tabs.executeScript(tab.id, { file: 'libs/jquery.js', allFrames: false });
+                    // chrome.tabs.executeScript(tab.id, { file: 'libs/jquery-confirm.min.js', allFrames: false });
+                    // if (a.purchased) chrome.tabs.executeScript(tab.id, { file: 'content/script.js', allFrames: false });
+                    // else chrome.tabs.executeScript(tab.id, { file: 'content/payment.js', allFrames: false });
                 });
             });
         });
@@ -35,32 +46,35 @@ class PopupActions {
 
 
     openPage() {
-        chrome.tabs.create({url: this.pageFacebook});
+        chrome.tabs.create({ url: this.pageFacebook });
     }
 
 
     initAction() {
 
-
-        chrome.tabs.getSelected(null, function (tab) {
-            this.currentUrl = tab.url;
-            if (this.isMessagePage(this.currentUrl) == true) {
-                $("#removeMessages").on("click", this.removeMessages.bind(this));
-                $("#openMessenger").addClass('disabled');
-                $("#openMessenger").attr('disabled', 'disabled');
-                $("#openMessenger").on("click", function (e) {
-                    e.preventDefault();
-                })
-            } else {
-                $("#openMessenger").on("click", this.openPage.bind(this))
-                $("#removeMessages").addClass('disabled');
-                $("#removeMessages").attr('disabled', 'disabled');
-                $("#removeMessages").on("click", function (e) {
-                    e.preventDefault();
-                    return
-                })
-            }
-        }.bind(this));
+        chrome.windows.getCurrent((currentWindow) => {
+            chrome.tabs.query({ active: true, windowId: currentWindow.id }, (activeTabs) => {
+                activeTabs.map((tab) => {
+                    var currentUrl = tab.url;
+                    if (this.isMessagePage(currentUrl) == true) {
+                        $("#removeMessages").on("click", this.removeMessages.bind(this));
+                        $("#openMessenger").addClass('disabled');
+                        $("#openMessenger").attr('disabled', 'disabled');
+                        $("#openMessenger").on("click", function (e) {
+                            e.preventDefault();
+                        })
+                    } else {
+                        $("#openMessenger").on("click", this.openPage.bind(this))
+                        $("#removeMessages").addClass('disabled');
+                        $("#removeMessages").attr('disabled', 'disabled');
+                        $("#removeMessages").on("click", function (e) {
+                            e.preventDefault();
+                            return
+                        })
+                    }
+                });
+            });
+        })
         $("#link").attr('href', `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`);
 
     }
